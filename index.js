669fa6fd2d01f10005b75541611bc3f18b31d36e
MaheshPandav <pandavmahesh490@gmail.com>
2023-12-10 21:56:39 +0530

    vercel configuration setting new

--- index.js
+++ index.js
@@ -8,13 +8,7 @@ const routes = require("./src/routes");
 const cors = require('cors');
 
 const jsonParser = bodyParser.json();
-const corsConfig = {
-  origin: '',
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE']
-}
-app.use(cors(corsConfig))
-app.options("", cors(corsConfig))
+app.use(cors());
 
 app.use(jsonParser);
 app.use("/api/v1", routes);
